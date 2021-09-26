34a85bea17d5d24465d68335a0319e0ec49de7c0
Denis Godoy <denisgodoy1@gmail.com>
2021-09-26 13:26:55 -0300

    update so_long

--- Makefile
+++ Makefile
@@ -8,19 +8,22 @@ SRC := main.c
 
 OBJ := $(SRC:%.c=%.o)
 
-LIB := -L ./minilibx -lmlx -lXext -lX11 
+LIB := -L ./42-libft -lft -L ./minilibx-linux -lmlx -lXext -lX11 
 
 all: $(NAME)
 
 $(NAME): clean $(OBJ)
-	$(MAKE) -C ./minilibx
+	$(MAKE) -C ./minilibx-linux
+	$(MAKE) -C ./42-libft
 	$(CC) $(FLAGS) $(OBJ) $(LIB) -o $(NAME)
 
 clean:
+	$(MAKE) -C ./42-libft clean
 	rm -rf $(OBJ)
 	rm -rf $(NAME)
 
 fclean: clean
-	$(MAKE) -C minilibx clean
+	$(MAKE) -C ./minilibx-linux clean
+	$(MAKE) -C ./42-libft fclean
 
 re: fclean all
--- so_long.h
+++ so_long.h
@@ -6,13 +6,14 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 22:44:47 by degabrie          #+#    #+#             */
-/*   Updated: 2021/09/26 00:17:30 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/09/26 13:19:26 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include "minilibx/mlx.h"
+# include "minilibx-linux/mlx.h"
+# include "42-libft/libft.h"
 # include <unistd.h>
 # include <stdio.h>
 #endif
