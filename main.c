5ecebb13a2d34ce33ac245a6bbb29cc900846fc6
Denis Godoy <denisgodoy1@gmail.com>
2021-09-27 02:55:04 -0300

    update so_long

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 22:45:16 by degabrie          #+#    #+#             */
-/*   Updated: 2021/09/26 13:21:27 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/09/27 02:53:02 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,25 @@ int	main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-
+	int		x;
+	int		temp_x;
+	int		y;
+	
+	x = 600;
+	temp_x = x;
+	y = 350;
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 600, 350, "so_long");
-	mlx_pixel_put(mlx_ptr, win_ptr, 300, 175, 0xFFFFF);
+	win_ptr = mlx_new_window(mlx_ptr, x, y, "so_long");
 	mlx_string_put(mlx_ptr, win_ptr, 300, 190, 0xF3F3F3, "hello 42");
+	while (x && y)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, --x, y, 0x25A60);
+		if (!x)
+		{
+			x = temp_x;
+			--y;
+		}
+	}
 	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	mlx_loop(mlx_ptr);
 }
