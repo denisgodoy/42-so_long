c0f87eec27d120192b1dd614f6f7cff19712483c
Denis Godoy <denisgodoy1@gmail.com>
2021-10-17 16:16:40 -0300

    update so_long

--- utils/validations/ft_max_resolution.c
+++ utils/validations/ft_max_resolution.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 03:46:00 by degabrie          #+#    #+#             */
-/*   Updated: 2021/10/17 04:15:13 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/10/17 16:11:38 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,14 @@ void	ft_max_resolution(t_game *game)
 	height = 0;
 	width = 0;
 	mlx_get_screen_size(game->mlx, &width, &height);
-	if ((game->map_utils.height * IMG_SIZE) >= (height / 1.1)
-		|| (game->map_utils.width * IMG_SIZE) >= (width / 1.05))
-		ft_error_handler("Map is bigger than screen resolution");
+	width = (width * 90) / 100;
+	height = (height * 80) / 100;
+	if ((game->map_utils.height * IMG_SIZE) > height
+		|| (game->map_utils.width * IMG_SIZE) > width)
+	{
+		printf("Max screen resolution: %dx%d\n", width, height);
+		printf("Map resolution: %dx%d\n", game->map_utils.width * IMG_SIZE,
+			game->map_utils.height * IMG_SIZE);
+		ft_error_handler("Map is way too big");
+	}
 }
